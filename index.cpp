9ebe29483714053a561759d2c17f2907e6ce1606
Lucas Araujo <104915888+Araujomann@users.noreply.github.com>
2022-11-23 13:57:38 -0300

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+void inicialpage();  //Área de protótipos das funções//
+void principalMenu();
+
+int main(){
+	option;
+	setlocale(LC_ALL, "Portuguese");
+	inicialpage();
+	
+	return 0;
+}
+
+void inicialpage(){
+	printf("\t\t\t\tSistema de Administração de Clientes Devedores - SACD\n\n");
+	printf("Seja bem-vindo(a) ao SACD, querido(a) cliente, o melhor sistema de agendamento de combranças da América Latina!");
+	printf("\n\n\n\n\t Aperte qualquer tecla para continuar.");
+	getchar();
+}
+
+void principalMenu(){
+	switch()
+	
+	
+}
